971c83bc35b467a25dc816b1ab99ab4f491f9195
Marijan Beg <marijan.beg@xfel.eu>
2018-03-14 16:32:03 +0100

    Update to 2.0a0_20170929a0 version.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 TCLTKVERSION=8.5
 
-# Compile OOMMF
+# Compile OOMMF.
 export OOMMF_TCL_CONFIG=${PREFIX}/lib/tclConfig.sh
 export OOMMF_TK_CONFIG=${PREFIX}/lib/tkConfig.sh
 cd oommf
@@ -11,13 +11,13 @@ tclsh${TCLTKVERSION} oommf.tcl pimake upgrade
 tclsh${TCLTKVERSION} oommf.tcl pimake
 tclsh${TCLTKVERSION} oommf.tcl +platform
 
-# Copy all OOMMF sources and compiled files into $PREFIX/opt/
+# Copy all OOMMF sources and compiled files into $PREFIX/opt/.
 install -d ${PREFIX}/opt/
 install -d ${PREFIX}/bin/
 cp -r ${SRC_DIR}/oommf ${PREFIX}/opt/
 
 # Create an executable called 'oommf' in ${PREFIX}/bin which
-# calls the OOMMF executable in $PREFIX/opt/
+# calls the OOMMF executable in $PREFIX/opt/.
 oommf_command=$(cat <<EOF
 #! /bin/bash
 export OOMMF_TCL_CONFIG=$PREFIX/lib/tclConfig.sh
