2d50f6b01c07fcb92b48ae5c258d39e3fb9a9408
Sergii Mamedov <finve.ua@gmail.com>
2019-01-06 18:40:09 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,18 +3,13 @@
 TCLTKVERSION=8.6
 
 # Compile OOMMF.
-export OOMMF_TCL_CONFIG=/opt/conda/lib/tclConfig.sh
-export OOMMF_TK_CONFIG=/opt/conda/lib/tkConfig.sh
+export OOMMF_TCL_CONFIG=${PREFIX}/lib/tclConfig.sh
+export OOMMF_TK_CONFIG=${PREFIX}/lib/tkConfig.sh
 
 # Check compiler.
 if [ $GXX ]
 then
   # Linux
-  echo "LS -LA LIB"
-  #ls -la /opt/conda/lib
-  echo "TCL CONFIG"
-  more /opt/conda/lib/tclConfig.sh
-  IFS=- read -a compiler_full_name <<< $GXX
   export COMPILER_NAME=g++ 
   ln ${BUILD_PREFIX}/bin/${HOST}-${COMPILER_NAME} ${BUILD_PREFIX}/bin/${COMPILER_NAME}
   ln ${BUILD_PREFIX}/bin/${HOST}-ld ${BUILD_PREFIX}/bin/ld
@@ -23,11 +18,9 @@ then
   mkdir -p /home/conda/feedstock_root/build_artifacts/tk_1542322308353/_build_env/bin/
   ln ${BUILD_PREFIX}/bin/${HOST}-ranlib /home/conda/feedstock_root/build_artifacts/tk_1542322308353/_build_env/bin/${HOST}-ranlib
   echo "LS -LA OOMMF"
-  #ls -la oommf
   tclsh oommf/oommf.tcl +platform
 else
   # MacOS
-  IFS=- read -a compiler_full_name <<< $CXX
   export COMPILER_NAME=clang++
   echo $COMPILER_NAME
 fi
