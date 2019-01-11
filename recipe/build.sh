6aefbbe59d5aafdf179188c5250e117ba0981b29
Min RK <benjaminrk@gmail.com>
2019-01-11 13:52:12 +0100

    fixes for conda compilers
    - respect CXXFLAGS in OOMMF_CPP
    - make symlinks for compiler executable due to invalid detections of clang
    - respect $LDFLAGS in platform config
    - workaround TCL_RANLIB buggy value

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,35 @@
 export OOMMF_TCL_CONFIG=${PREFIX}/lib/tclConfig.sh
 export OOMMF_TK_CONFIG=${PREFIX}/lib/tkConfig.sh
 
+# ensure compiler executables exist in build prefix
+# due to invalid assumptions about compiler executable names
+# in oommf build config
+if [[ "$(uname)" == "Darwin" ]]; then
+  cpp_bin="clang++"
+  oommf_platform="darwin"
+else
+  cpp_bin="g++"
+  oommf_platform="linux-x86_64"
+fi
+# scrub debug-prefix-map which causes problems
+export CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-fdebug\-prefix\-map[^ ]*@@g')
+# create compiler command with cxx flags
+export OOMMF_CPP="$cpp_bin -c $CXXFLAGS"
+
+test -f "$BUILD_PREFIX/bin/$cpp_bin" || ln -s "$CC" "$BUILD_PREFIX/bin/$cpp_bin"
+
+# make sure LDFLAGS are respected
+sed -i -e "/# START EDIT HERE/a\\
+\$config SetValue program_linker_extra_args {$LDFLAGS}
+" oommf/config/platforms/$oommf_platform.tcl
+
+# fix possibly incorrect TCL_RANLIB
+if [[ ! -z "$RANLIB" ]]; then
+    sed -i -e '/# START EDIT HERE/a\
+$config SetValue TCL_RANLIB $env(RANLIB)
+' oommf/config/platforms/$oommf_platform.tcl
+fi
+
 make build-with-dmi-extension-all -j${CPU_COUNT}
 
 # Copy all OOMMF sources and compiled files into $PREFIX/opt/.
@@ -14,12 +43,10 @@ cp -r ${SRC_DIR}/oommf ${PREFIX}/opt/
 
 # Create an executable called 'oommf' in ${PREFIX}/bin which
 # calls the OOMMF executable in $PREFIX/opt/.
-oommf_command=$(cat <<EOF
+cat > ${PREFIX}/bin/oommf <<EOF
 #! /bin/bash
 export OOMMF_TCL_CONFIG=$PREFIX/lib/tclConfig.sh
 export OOMMF_TK_CONFIG=$PREFIX/lib/tkConfig.sh
 $PREFIX/bin/tclsh $PREFIX/opt/oommf/oommf.tcl "\$@"
 EOF
-)
-echo "$oommf_command" > ${PREFIX}/bin/oommf
 chmod a+x ${PREFIX}/bin/oommf
