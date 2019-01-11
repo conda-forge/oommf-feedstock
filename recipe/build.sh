d26d104c187f5aa26b0a2c93330949fa85b1086d
Min RK <benjaminrk@gmail.com>
2019-01-11 13:43:21 +0100

    fix use of $PREFIX
    $SYS_PREFIX isn't the right thing

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-TCLTKVERSION=8.6
-
 # Compile OOMMF.
-export OOMMF_TCL_CONFIG=${SYS_PREFIX}/lib/tclConfig.sh
-export OOMMF_TK_CONFIG=${SYS_PREFIX}/lib/tkConfig.sh
-ls -la ${BUILD_PREFIX}/bin
-echo "PATH"
-echo $PATH
-make build-with-dmi-extension-all
+export OOMMF_TCL_CONFIG=${PREFIX}/lib/tclConfig.sh
+export OOMMF_TK_CONFIG=${PREFIX}/lib/tkConfig.sh
+
+make build-with-dmi-extension-all -j${CPU_COUNT}
 
 # Copy all OOMMF sources and compiled files into $PREFIX/opt/.
 #echo "INSTALL SOFTWARE ======"
-#install -d ${SYS_PREFIX}/opt/
-#install -d ${SYS_PREFIX}/bin/
-#cp -r ${SRC_DIR}/oommf ${SYS_PREFIX}/opt/
+install -d ${PREFIX}/opt/
+install -d ${PREFIX}/bin/
+cp -r ${SRC_DIR}/oommf ${PREFIX}/opt/
 
 # Create an executable called 'oommf' in ${PREFIX}/bin which
 # calls the OOMMF executable in $PREFIX/opt/.
-#oommf_command=$(cat <<EOF
-##! /bin/bash
-#export OOMMF_TCL_CONFIG=$SYS_PREFIX/lib/tclConfig.sh
-#export OOMMF_TK_CONFIG=$SYS_PREFIX/lib/tkConfig.sh
-#$PREFIX/bin/tclsh${TCLTKVERSION} $SYS_PREFIX/opt/oommf/oommf.tcl "\$@"
-#EOF
-#)
-#echo "$oommf_command" > ${SYS_PREFIX}/bin/oommf
-#chmod a+x ${SYS_PREFIX}/bin/oommf
+oommf_command=$(cat <<EOF
+#! /bin/bash
+export OOMMF_TCL_CONFIG=$PREFIX/lib/tclConfig.sh
+export OOMMF_TK_CONFIG=$PREFIX/lib/tkConfig.sh
+$PREFIX/bin/tclsh $PREFIX/opt/oommf/oommf.tcl "\$@"
+EOF
+)
+echo "$oommf_command" > ${PREFIX}/bin/oommf
+chmod a+x ${PREFIX}/bin/oommf
