76c55e5f3519bc335a39dc2dca20a0163c9b9b31
Martin Lang <m.lang@soton.ac.uk>
2021-10-14 18:11:31 +0200

    Clean up.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,62 +1,23 @@
-cd
-dir
-dir %PREFIX%
-dir %PREFIX%\opt
-dir %PREFIX%\Library
-
 set OOMMF_TCL_CONFIG=%BUILD_PREFIX%\Library\lib\tclConfig.sh
 set OOMMF_TK_CONFIG=%BUILD_PREFIX%\Library\lib\tkConfig.sh
 
-set OOMMF_ROOT=oommf
+set OOMMF_ROOT=%cd%\oommf
 set OOMMFTCL=%OOMMF_ROOT%\oommf.tcl
-
-dir /s /b /a-d "tclConfig.sh"
-
-rem mkdir %PREFIX%\opt\oommf
-rem xcopy * %PREFIX%\opt\oommf /e
-
-rem # create compiler command with cxx flags
-rem export OOMMF_CPP="$cpp_bin -c $CXXFLAGS"
-
-rem test -f "$BUILD_PREFIX/bin/$cpp_bin" || ln -s "$CC" "$BUILD_PREFIX/bin/$cpp_bin"
-
-rem # make sure LDFLAGS are respected
-rem # does not seem to have the desired effect
-rem sed -i -e '/# START EDIT HERE/a\
-rem $config SetValue program_linker_extra_args $env(LDFLAGS)
-rem ' oommf/config/platforms/$oommf_platform.tcl
-
-rem # fix possibly incorrect TCL_RANLIB
-rem if [[ ! -z "$RANLIB" ]]; then
-rem     sed -i -e '/# START EDIT HERE/a\
-rem $config SetValue TCL_RANLIB $env(RANLIB)
-rem ' oommf/config/platforms/$oommf_platform.tcl
-rem fi
-
-rem make build -j${CPU_COUNT}
 
 cd %OOMMF_ROOT%
 
-dir
-tclsh oommf.tcl pimake distclean
+tclsh %OOMMFTCL% pimake distclean
 if errorlevel 1 exit 1
-tclsh oommf.tcl pimake upgrade
-if errorlevel 1 exit 1
-tclsh oommf.tcl pimake
-if errorlevel 1 exit 1
-tclsh oommf.tcl pimake objclean
-if errorlevel 1 exit 1
-dir
 
-rem # remove TCL_RANLIB and program_linker_extra_args from config
-rem sed -i -e '/$config SetValue TCL_RANLIB $env(RANLIB)/d' oommf/config/platforms/$oommf_platform.tcl
-rem sed -i -e '/$config SetValue program_linker_extra_args $env(LDFLAGS)/d' oommf/config/platforms/$oommf_platform.tcl
+tclsh %OOMMFTCL% pimake upgrade
+if errorlevel 1 exit 1
 
+tclsh %OOMMFTCL% pimake
+if errorlevel 1 exit 1
 
-rem Copy all OOMMF sources and compiled files into $PREFIX/opt/.
+tclsh %OOMMFTCL% pimake objclean
+if errorlevel 1 exit 1
+
+rem Copy all OOMMF sources and compiled files into %PREFIX%\opt\.
 mkdir %PREFIX%\opt\oommf
-if errorlevel 1 exit 1
-dir %PREFIX%\opt\oommf
 xcopy %SRC_DIR%\oommf %PREFIX%\opt\oommf /e
-if errorlevel 1 exit 1
-dir %PREFIX%\opt\oommf
