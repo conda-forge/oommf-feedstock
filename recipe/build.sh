192f31461aebc53682d88fa0ed877de68f95a05e
Martin Lang <martin.lang@mpsd.mpg.de>
2025-07-25 19:52:20 +0200

    Remove -framework UniformTypeIdentifiers

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,9 +10,7 @@ export OOMMF_TK_CONFIG=${PREFIX}/lib/tkConfig.sh
 if [[ "$(uname)" == "Darwin" ]]; then
   cpp_bin="clang++"
   oommf_platform="darwin"
-  sed -i.bak \
-    's/-framework UniformTypeIdentifiers/-weak_framework UniformTypeIdentifiers/g' \
-    "$PREFIX/lib/tkConfig.sh"
+  sed -i.bak 's/-framework UniformTypeIdentifiers//g'  "${PREFIX}/lib/tkConfig.sh"
 else
   cpp_bin="g++"
   oommf_platform="linux-x86_64"
